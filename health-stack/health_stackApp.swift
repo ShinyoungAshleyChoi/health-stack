b82b52170792680092b8255c72abd4797ac22cf8
shinyoungashleychoi <choishin0@gmail.com>
2025-11-10 15:46:35 +0900

    Initial Commit

---
+++ health-stack/health_stackApp.swift
@@ -0,0 +1,17 @@
+//
+//  health_stackApp.swift
+//  health-stack
+//
+//  Created by shinyoung choi on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct health_stackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ health-stackTests/health_stackTests.swift
@@ -0,0 +1,17 @@
+//
+//  health_stackTests.swift
+//  health-stackTests
+//
+//  Created by shinyoung choi on 11/10/25.
+//
+
+import Testing
+@testable import health_stack
+
+struct health_stackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
